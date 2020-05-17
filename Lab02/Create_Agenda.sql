93619a2736d20eac9917db26fa71eddbd6f2d29d
sharon1160 <49489913+sharon1160@users.noreply.github.com>
2020-05-17 13:18:50 -0500

    Add files via upload

---
+++ Lab02/Create_Agenda.sql
@@ -0,0 +1,35 @@
+create table Personas(
+ID varchar(10),
+Nombre varchar(20) not null,
+Apellidos varchar(150) not null,
+Fech_naci date not null,
+Domicilio varchar(60) not null,
+primary key(ID)
+);
+
+create table Teléfonos(
+Número int,
+Tipo varchar(20) not null,
+primary key(Número)
+);
+
+create table Personas_Teléfonos(
+ID_Personas varchar(10),
+Número_Teléfonos int,
+constraint foreign key(ID_Personas) references Personas(ID)
+on delete cascade
+on update cascade,
+constraint foreign key(Número_Teléfonos) references Teléfonos(Número)
+on delete cascade
+on update cascade
+);
+
+create table Correos(
+Correo varchar(150),
+Tipo varchar(150) not null,
+ID_Personas varchar(10),
+primary key(Correo),
+constraint foreign key(ID_Personas) references Personas(ID)
+on delete cascade
+on update cascade
+);
